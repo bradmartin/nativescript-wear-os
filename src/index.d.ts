9992196e6d06008e8e8c81ffb9f21c251c21d46e
Brad Martin <bradwaynemartin@gmail.com>
2019-03-14 18:08:45 -0500

    fix manifest for confirmationActivity

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,7 @@
 export * from './packages';
+export * from './packages/utils';
+export * from './packages/box-inset-layout';
+export * from './packages/circular-progress-layout';
+export * from './packages/listview';
+export * from './packages/swipe-dismiss-layout';
+export * from './packages/wear-os-layout';
