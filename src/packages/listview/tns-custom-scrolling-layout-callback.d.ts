b7689f95b99c9edb6e77a080ca2ee2fb06b03cc5
Brad Martin <bradwaynemartin@gmail.com>
2019-07-31 15:00:37 -0500

    bump for bad publish

--- src/packages/listview/tns-custom-scrolling-layout-callback.d.ts
+++ src/packages/listview/tns-custom-scrolling-layout-callback.d.ts
@@ -2,5 +2,5 @@ export declare class TNS_CustomScrollingLayoutCallback extends androidx.wear.wid
     .WearableLinearLayoutManager.LayoutCallback {
     private static MAX_ICON_PROGRESS;
     constructor();
-    onLayoutFinished(child: android.view.View, parent: any): void;
+    onLayoutFinished(child: android.view.View, parent: androidx.recyclerview.widget.RecyclerView): void;
 }
